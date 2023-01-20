312528772502101f66b9db63f9e37efe4e85f657
Yassine <yass2400012@gmail.com>
2023-01-20 17:42:07 -0500

    change

--- modules/moviesDB.js
+++ modules/moviesDB.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const movieSchema = new Schema({
   plot: String,
   genres: [String],
--- server.js
+++ server.js
@@ -4,8 +4,8 @@
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Yassine Lebbar Student ID: 159804210 Date: 18/01/2023
-*  Cyclic Link: _______________________________________________________________
+*  Name: Yassine Lebbar Student ID: 159804210 Date: 19/01/2023
+*  Cyclic Link: 
 *
 ********************************************************************************/ 
 
@@ -14,68 +14,101 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv').config(); 
+require('dotenv').config(); 
 const mongoose = require('mongoose');
 const MoviesDB = require("./modules/moviesDB.js");
 const db = new MoviesDB();
 const app = express();
-const HTTP_PORT = process.env.PORT || 8080;
+const HTTP_PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: 'API Listening' });
-});
 
+//get default Page
+app.get("/", function (req, res) {
+    res.send("API Listening");
+  });
   
-app.post('/api/movies', (req, res) => {
-    db.addNewMovie(req.body).then(() => {
-        res.status(201).send("New movie successfully added to database");
-    }).catch((err) => {
-        res.status(204).send(err);
-    })
-});
+  //post
+  app.post("/api/movies", async function (req, res) {
+    //try and catch method
+    try {
+      if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ error: "No movie data" });
+      }
+      const data = await db.addNewMovie(req.body);
+      res.status(201).json(data);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
   
-app.get('/api/movies/', (req, res) => {
-    db.getAllMovies(req.query.page, req.query.perPage, req.query.title).then((movies) => {
-        res.status(200).json(movies);
-    }).catch((err) => {
-        res.status(204).send(err);
-
-    })
-});
+  //get movies
+  app.get("/api/movies", async function (req, res) {
+    try {
+      const data = await db.getAllMovies(
+        req.query.page,
+        req.query.perPage,
+        req.query.title || null
+      );
+      if (data.length === 0) {
+        return res.status(204).send();
+        console.log('data length is zero');
+      }
+      res.json(data);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
   
-app.get('/api/movies/:id', (req, res) => {
-    db.getMovieById(req.params.id).then((movie) => {
-        res.status(200).json(movie);
-    }).catch((err) => {
-        res.status(204).send(err);
-    })
-});
+  //get movies id
+  app.get("/api/movies/:_id", async function (req, res) {
+    try {
+      const data = await db.getMovieById(req.params._id);
+      //for empty
+      if (!data) {
+        return res.status(400).json({ error: "Movie not found." });
+      }
+      res.send(data);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
   
-app.put('/api/movie/:id', (req, res) => {
-    db.updateMovieById(req.body, req.params.id).then(() => {
-        res.status(200).send('Movie updated successfully');
-    }).catch((err) => {
-        res.status(500).send(err);
-    })
-});
+  //delete
+  app.delete("/api/movies/:_id", async function (req, res) {
+    try {
+      const movie = await db.getMovieById(req.params._id);
+      await db.deleteMovieById(req.params._id);
+      res.json({ success: `Movie - ${movie.title} deleted!` });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
   
-app.delete('/api/movies/:id', (req, res) => {
-    db.deleteMovieById(req.params.id).then(() => {
-        res.status(200).send("Movie deleted :" + req.params.id);
-    }).catch((err) => {
-        res.status(500).send(err);
+  //put movie id
+  app.put("/api/movie/:_id", async function (req, res) {
+    try {
+      if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ error: "No data provided to update." });
+      }
+      const data = await db.updateMovieById(req.body, req.params._id);
+      res.json({ success: "Movie updated!" });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+  
+  //initialize
+  db.initialize(process.env.MONGODB_CONN_STRING)
+    .then(() => {
+      app.listen(HTTP_PORT, function () {
+        console.log(`Server listening on port ${HTTP_PORT}`);
+      });
     })
-});
-
-db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
-    app.listen(HTTP_PORT, ()=>{
-      console.log(`server listening on: ${HTTP_PORT}`);
+    .catch(function (err) {
+      console.log(err.message);
     });
-  }).catch((err)=>{
-    console.log(err);
-});
-   
+  
